bfed20c770ab56e3bd69ee2b5c8b03dc586955b8
Paulo Salvatore <salvatoregames@gmail.com>
2023-01-20 10:16:50 -0300

    Pegando `Rigidbody2D` no `Start` do Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Precisamos obter o Rigidbody 2D que está no GameObject do Player
+    // Para isso, precisamos definir uma variável que seja acessada tanto
+    // pelo Start(), quanto pelo Update()
+
+    // Para declarar uma variável na C#, utilizamos a seguinte estrutura:
+    // TIPO_VARIAVEL NOME_VARIAVEL;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Como o Rigidbody2D está no mesmo objeto de jogo que o script Player está,
+        // podemos utilizar a declaração GetComponent<TIPO_COMPONENTE>() para obter
+        // o Rigidbody do jogador em si
+        rb = GetComponent<Rigidbody2D>();
+        print(rb);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
